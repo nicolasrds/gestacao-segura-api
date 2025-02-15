6e86b0eda04770540dccb017a18c72820b808f71
Yuri Alves <yuri.o.alves2@outlook.com>
2025-02-15 12:02:34 -0300

    Primeiro commit

---
+++ src/main/java/com/gestacao/segura/GestacaoSeguraApplication.java
@@ -0,0 +1,13 @@
+package com.gestacao.segura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestacaoSeguraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestacaoSeguraApplication.class, args);
+	}
+
+}
