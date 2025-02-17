ea08a3c4793d49b41ef0d1d6a3780d6be2cb9e1e
Yuri Oliveira Alves <yuri.o.alves2@outlook.com>
2025-02-17 20:36:12 -0300

    Criando a entidade gestante

---
+++ src/main/java/com/gestacao/segura/entities/Gestante.java
@@ -0,0 +1,64 @@
+package com.gestacao.segura.entities;
+
+import com.gestacao.segura.dtos.GestanteRequestDTO;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "gestantes")
+@Entity
+public class Gestante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 14)
+    private String cpf;
+
+    @Column(nullable = false, length = 100)
+    private String nomeGestante;
+
+    @Column(nullable = false)
+    private LocalDate dataNascimento;
+
+    @Column(nullable = false, unique = true)
+    private Integer numeroCartaoSus;
+
+    @Column(length = 15)
+    private String telefone;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Embedded
+    private Endereco endereco;
+
+    @Column(nullable = false, length = 3)
+    private String tipagemSanguinea;
+
+    @Column(nullable = false, length = 3)
+    private String fatorRh;
+
+    public void update(GestanteRequestDTO dto) {
+        this.cpf = dto.cpf();
+        this.nomeGestante = dto.nomeGestante();
+        this.dataNascimento = dto.dataNascimento();
+        this.numeroCartaoSus = dto.numeroCartaoSus();
+        this.telefone = dto.telefone();this.senha = dto.senha();
+        this.email = dto.email();
+        this.senha = dto.senha();
+        this.endereco = dto.endereco();
+        this.tipagemSanguinea = dto.tipagemSanguinea();
+        this.fatorRh = dto.fatorRh();
+    }
+}
---
+++ src/main/java/com/gestacao/segura/services/GestanteService.java
@@ -0,0 +1,55 @@
+package com.gestacao.segura.services;
+
+import com.gestacao.segura.dtos.GestanteRequestDTO;
+import com.gestacao.segura.dtos.GestanteResponseDTO;
+import com.gestacao.segura.entities.Gestante;
+import com.gestacao.segura.mappers.GestanteMapper;
+import com.gestacao.segura.repositories.GestanteRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GestanteService {
+
+    private final GestanteRepository gestanteRepository;
+
+    public GestanteService(GestanteRepository gestanteRepository) {
+        this.gestanteRepository = gestanteRepository;
+    }
+
+    public Page<GestanteResponseDTO> findAll(Pageable pageable){
+        Page<Gestante> gestantes = gestanteRepository.findAll(pageable);
+
+        return gestantes.map(GestanteMapper::toDto);
+    }
+
+    public GestanteResponseDTO findById(Long id){
+        Gestante gestante = gestanteRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Não foi possivel encontrar o id"));
+
+        return GestanteMapper.toDto(gestante);
+    }
+
+    public GestanteResponseDTO save(GestanteRequestDTO dto){
+        Gestante gestante = GestanteMapper.toEntity(dto);
+        gestante = gestanteRepository.save(gestante);
+
+        return GestanteMapper.toDto(gestante);
+    }
+
+    public GestanteResponseDTO update(Long id, GestanteRequestDTO dto){
+        Gestante gestante = gestanteRepository.getReferenceById(id);
+        gestante.update(dto);
+
+        return GestanteMapper.toDto(gestante);
+    }
+
+    public void delete(Long id) {
+        if (!gestanteRepository.existsById(id)) {
+            throw new EntityNotFoundException("Não foi possivel encontrar o id");
+        } else {
+            gestanteRepository.deleteById(id);
+        }
+    }
+}
